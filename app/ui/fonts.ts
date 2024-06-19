762759edce4c63217ae8fa5efa2e167f8ec4ea60
codepil-slab <piyush.v@solutelabs.com>
2024-06-19 10:59:26 +0530

    added authentication, invoices CRUD operation

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,7 +2,9 @@ import { Inter, Lusitana } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
 
-export const lusitana = Lusitana({
-    subsets: ['latin'],
-    weight: ['400', '700'],
-});
+export const lusitana = Inter({ subsets: ['latin'] });
+
+// export const lusitana = Lusitana({
+//     subsets: ['latin'],
+//     weight: ['400', '700'],
+// });
